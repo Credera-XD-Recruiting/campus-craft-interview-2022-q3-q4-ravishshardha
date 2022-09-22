50712e4f0f8ddf363e62fe6c2e830179562ad034
ravish shardha <ravishshardha26@gmail.com>
2022-09-22 11:35:54 -0500

    added support for “Friends” sorting alphabetically by “Top Friends” + LAST name

--- src/components/profile-friends.js
+++ src/components/profile-friends.js
@@ -1,6 +1,52 @@
 import { removeChildNodes } from "../utils";
 
 /**
+ * Function which compares two strings
+ *
+ * @param {string} a first string to compare
+ * @param {string} b second string to compare
+ * @return {int} integer denoting which of the string is alphabetically bigger
+ */
+const compareStrings = (a, b) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+
+  return 0;
+}
+
+/**
+ * Function which is a custom compare as the list needs to be sorted by last names. 
+ *
+ * @param {object} a first friend to compare
+ * @param {object} b second friend to compare
+ * @return {int} integer denoting which of the friend last name is alphabetically bigger
+ */
+const customCompare = (a, b) => {
+  // split on space to get first, middle, and last name separated
+  const splitA = a.name.split(" ");
+  const splitB = b.name.split(" ");
+  // get the value (last name) which is located at last index
+  const lastA = splitA[splitA.length - 1];
+  const lastB = splitB[splitB.length - 1];
+
+  // if both last names are same, then use first name to sort alphabetically else use last name
+  return lastA === lastB ?
+    compareStrings(splitA[0], splitB[0]) :
+    compareStrings(lastA, lastB);
+}
+
+const sortFriendsList= (friendsList) => {
+  // top friends 
+  const topFriends = friendsList.filter((friend) => friend?.topFriend);
+  // all other friends who are not top friends
+  const otherFriends = friendsList.filter((friend) => !friend?.topFriend);
+  // sort by last name using custom compare
+  const otherFriendsSortByLastName = (otherFriends || []).sort(customCompare);
+  //merging both of the list
+  return [...topFriends, ...otherFriendsSortByLastName];
+}
+
+/**
  * Function which generates a single list-item node based on a dataset
  *
  * @param {object} data data containing attributes of a listItem
@@ -49,9 +95,9 @@ export const generateFriendsListFromTemplate = (resultsData) => {
 
   if (resultsData.friends && resultsData.friends.length > 0) {
     removeChildNodes(friendsListSection);
-
-    for (let i = 0; i < resultsData.friends.length; i++) {
-      const friendsNode = generateListItemNode(resultsData.friends[i]);
+    const friends = sortFriendsList(resultsData.friends);
+    for (let i = 0; i < friends.length; i++) {
+      const friendsNode = generateListItemNode(friends[i]);
       friendsListSection.appendChild(friendsNode);
     }
   }
