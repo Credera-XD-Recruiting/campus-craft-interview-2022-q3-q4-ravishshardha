19bb6a08c7b815c6abf123f517a2fa14510d221b
ravish shardha <ravishshardha26@gmail.com>
2022-09-21 23:03:21 -0500

    US # 2-111 added support for “Groups” should be colored according to “activity”  by updating the background color

--- src/components/profile-group.js
+++ src/components/profile-group.js
@@ -6,6 +6,36 @@ const activityStates = {
   moderate: "moderate",
   low: "low",
 };
+
+const colors = {
+  active:  "#52C1AD",
+  inactive: "#C4C4C4",
+  moderate: "#58B1C9",
+  low: "#C152A2",
+};
+
+/**
+ * Function which returns background color based on group activity
+ *
+ * @param {activity} activity activity of the group
+ * @return {string} color for the group activity
+ */
+const getBackgroundColorBasedOnGroupActivity = (activity) => {
+  switch(activity){
+    case activityStates.active:
+      return colors.active;
+    
+    case activityStates.moderate:
+      return colors.moderate;
+
+    case activityStates.low:
+      return colors.low;
+
+    default:
+      return colors.inactive;
+  }
+}
+
 /**
  * Function which generates a single Card node based on a dataset
  *
@@ -13,7 +43,7 @@ const activityStates = {
  * @return {Node} generated markup for a card
  */
 const generateCardNode = (data) => {
-  const { name, href, image } = data;
+  const { name, href, image, activity } = data;
   const templateId = "profile-group-results-item-template";
   const resultCardTemplate = document.getElementById(templateId);
   const clone = document.importNode(resultCardTemplate.content, true);
@@ -26,6 +56,8 @@ const generateCardNode = (data) => {
   titleNode.innerHTML = `${name}`;
   referenceNode.href = href;
   groupImageNode.src = image;
+  // update the background color of the card based on group activity
+  referenceNode.style.backgroundColor = getBackgroundColorBasedOnGroupActivity(activity);
 
   return clone;
 };
